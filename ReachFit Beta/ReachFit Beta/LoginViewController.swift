5261df8e161dee3bee3c8ada602dd7e28097111f
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-13 10:56:41 -0700

    ReachFit Beta Uploads. Both Teacher and Student Applications. Beginning Stages.

---
+++ ReachFit Beta/ReachFit Beta/LoginViewController.swift
@@ -0,0 +1,74 @@
+//
+//  LoginViewController.swift
+//  ReachFit Beta
+//
+//  Created by Stefan DeClerck on 6/8/15.
+//  Copyright (c) 2015 ReachFit. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    @IBAction func login(sender: AnyObject) {
+        
+        PFUser.logInWithUsernameInBackground(emailTextField.text, password:passwordTextField.text) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                if user?.objectForKey("isStudent")! as! NSObject == "true" {
+                    CurrentUserInfo.currentUserUsername = self.emailTextField.text
+                    CurrentUserInfo.currentUserEmail = user!.objectForKey("email") as! String
+                    CurrentUserInfo.currentUserAge = user!.objectForKey("userAge") as! String
+                    println("success")
+                    self.performSegueWithIdentifier("finishLogin", sender: self)
+                } else  {
+                    let alertController = UIAlertController(title: "Alert", message:
+                        "You do not have a student account, please sign in using the teacher account.", preferredStyle: UIAlertControllerStyle.Alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
+                    
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                }
+            } else {
+                let alertController = UIAlertController(title: "Alert", message:
+                    "Login credentials invalid", preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
+                
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        
+        self.emailTextField.becomeFirstResponder()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

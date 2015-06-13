3d8800a0b2d7a1ead8a3be26666a1adf220774a6
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-13 12:29:11 -0700

    update

---
+++ ReachFit Teacher Beta/ReachFit Teacher Beta/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  ReachFit Teacher Beta
+//
+//  Created by Stefan DeClerck on 6/13/15.
+//  Copyright (c) 2015 ReachFit. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

b9bda6458dc8f19fcb348828b1653922cc5228bd
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-16 13:50:38 -0700

    commit

---
+++ ReachFit Teacher Beta/ReachFit Teacher Beta/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddViewController.swift
+//  ReachFit Teacher Beta
+//
+//  Created by Stefan DeClerck on 6/16/15.
+//  Copyright (c) 2015 ReachFit. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var classNameTextField: UITextField!
+    @IBOutlet weak var workoutTypeTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var timeTextField: UITextField!
+    
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

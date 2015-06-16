b8e3f35acc0b753b501bf208b8f5c1e6c72e86a0
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-16 16:49:25 -0700

    commit

--- ReachFit Teacher Beta/ReachFit Teacher Beta/AddViewController.swift
+++ ReachFit Teacher Beta/ReachFit Teacher Beta/AddViewController.swift
@@ -15,9 +15,31 @@ class AddViewController: UIViewController {
     @IBOutlet weak var workoutTypeTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
-     
+    
     @IBAction func done(sender: AnyObject) {
+        var currentUsername: PFUser = PFUser.currentUser()!
+        var newClass = PFObject(className: "Classes")
+        newClass["instructor"] = currentUsername.username
+        newClass["classFullName"] = classNameTextField.text
+        newClass["classTopic"] = workoutTypeTextField.text
+        newClass["classDescription"] = descriptionTextField.text
+        newClass["classDate"] = dateTextField.text
         
+        if classNameTextField.text == "" || workoutTypeTextField.text == "" || descriptionTextField.text == "" || dateTextField == "" {
+            var alert = UIAlertController(title: "Error", message: "Make sure to fill in all fields", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            newClass.saveInBackgroundWithBlock { (success, error) -> Void in
+                if error != nil {
+                    var alert = UIAlertController(title: "Error", message: "There is an error with the given information", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                } else {
+                    
+            }
+            }
+        }
     }
     
     override func viewDidAppear(animated: Bool) {

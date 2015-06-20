e1fedf3550afe31da91084a82b7ee92e3d3fa6e8
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-20 09:09:40 -0700

    commit

---
+++ Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/StartingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StartingViewController.swift
+//  ReachFit Athlete
+//
+//  Created by Stefan DeClerck on 6/20/15.
+//  Copyright (c) 2015 ReachFit. All rights reserved.
+//
+
+import Cocoa
+
+class StartingViewController: UIViewController {
+
+}

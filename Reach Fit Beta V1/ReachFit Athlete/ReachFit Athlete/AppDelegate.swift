67a600ba3becf6e85563d7a489358ad3466a1891
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-19 20:54:18 -0700

    commit

--- Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/AppDelegate.swift
+++ Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Parse.setApplicationId("sCsLLNAEyIqKPb7CtvGwAqNYQWR3XRUku6n6D9ew", clientKey: "Rm9kqIGX9othZGwn1hrEXEKcly1uF0kUZhy2TeH1")
+        
         return true
     }
 

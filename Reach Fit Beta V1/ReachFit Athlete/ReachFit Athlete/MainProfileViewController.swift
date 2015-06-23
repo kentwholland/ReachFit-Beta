d579691e293a362e4c60dc12d7509471ce5f96ea
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-23 01:00:42 -0700

    commit

--- Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/MainProfileViewController.swift
+++ Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/MainProfileViewController.swift
@@ -9,13 +9,65 @@
 import UIKit
 import Parse
 
-class MainProfileViewController: UIViewController {
+class MainProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var profileImageView: UIImageView!
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+        self.dismissViewControllerAnimated(true, completion:nil)
+        
+        let user = PFUser.currentUser()
+        
+        profileImageView.image = image
+        
+        let imageData = UIImagePNGRepresentation(profileImageView.image)
+        let imageFile = PFFile(name: "image.png", data: imageData)
+        
+        user?.setObject(imageFile, forKey: "profilePicture")
+        
+        user!.saveInBackgroundWithBlock { (success, error) -> Void in
+            if error == nil {
+                
+                println("image upload success")
+                
+            } else {
+                
+                var alert = UIAlertController(title: "Error", message: "Could not set profile picture", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction((UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+                    
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                    
+                })))
+                
+                self.presentViewController(alert, animated: true, completion: nil)
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func profileImageViewTapped(sender: AnyObject) {
+        
+        var image = UIImagePickerController()
+        
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2;
+        profileImageView.clipsToBounds = true;
+        profileImageView.userInteractionEnabled = true
+        
         let pictureObject: AnyObject? = CurrentUserInfo.currentUserProfilePic
         pictureObject!.getDataInBackgroundWithBlock { (imageData: NSData?, error:NSError?) -> Void in
             if error == nil {

c5d9bec1634632ed867790174ec63962f057fed1
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-23 12:22:09 -0700

    commit

---
+++ Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/UserSettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  UserSettingsViewController.swift
+//  ReachFit Athlete
+//
+//  Created by Stefan DeClerck on 6/23/15.
+//  Copyright (c) 2015 ReachFit. All rights reserved.
+//
+
+import UIKit
+
+class UserSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

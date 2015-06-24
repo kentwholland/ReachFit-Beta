0fa54fb88ad8921db792b34460b9b82091451247
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-24 15:37:05 -0700

    commit

---
+++ Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/UpcomingTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpcomingTableViewController.swift
+//  ReachFit Athlete
+//
+//  Created by Stefan DeClerck on 6/24/15.
+//  Copyright (c) 2015 ReachFit. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+        
+    }
+
+
+}

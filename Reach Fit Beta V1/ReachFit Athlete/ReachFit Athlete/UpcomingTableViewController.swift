67237941ef725e528776e11c5544281f149809b3
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-06-24 17:31:41 -0700

    commit

--- Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/UpcomingTableViewController.swift
+++ Reach Fit Beta V1/ReachFit Athlete/ReachFit Athlete/UpcomingTableViewController.swift
@@ -17,14 +17,24 @@ class UpcomingTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
         return 1
-        
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
         
+        return 1
     }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var  cell:UpcomingTableViewCell? = tableView.dequeueReusableCellWithIdentifier("upcomingTableViewCellBox") as? UpcomingTableViewCell
 
-
+        if cell == nil {
+            let nib:Array = NSBundle.mainBundle().loadNibNamed("TableViewCell", owner: self, options: nil)
+            cell = nib[0] as? UpcomingTableViewCell
+        }
+        
+        return cell!
+    }
+    
 }

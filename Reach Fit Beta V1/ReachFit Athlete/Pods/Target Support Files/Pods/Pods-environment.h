76037227cca8cfc2dc1eb02a0604ed7353fa359b
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-07-09 13:12:41 -0700

    add NSDate Podfile

---
+++ Reach Fit Beta V1/ReachFit Athlete/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Timepiece
+#define COCOAPODS_POD_AVAILABLE_Timepiece
+#define COCOAPODS_VERSION_MAJOR_Timepiece 0
+#define COCOAPODS_VERSION_MINOR_Timepiece 3
+#define COCOAPODS_VERSION_PATCH_Timepiece 4
+

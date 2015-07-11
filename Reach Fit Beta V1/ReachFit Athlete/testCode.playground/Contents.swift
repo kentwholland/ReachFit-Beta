850954fc81d2918332e63958033ddf461dfd625d
Stefan DeClerck <stefan.declerck26@gmail.com>
2015-07-11 10:41:10 -0700

    commit

--- Reach Fit Beta V1/ReachFit Athlete/testCode.playground/Contents.swift
+++ Reach Fit Beta V1/ReachFit Athlete/testCode.playground/Contents.swift
@@ -1,31 +1,3 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-func <=(lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.timeIntervalSince1970 <= rhs.timeIntervalSince1970
-}
-func >=(lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.timeIntervalSince1970 >= rhs.timeIntervalSince1970
-}
-func >(lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.timeIntervalSince1970 > rhs.timeIntervalSince1970
-}
-func <(lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.timeIntervalSince1970 < rhs.timeIntervalSince1970
-}
-func ==(lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.timeIntervalSince1970 == rhs.timeIntervalSince1970
-}
-
-let date0 = NSDate(timeIntervalSince1970: 0)
-let date1 = NSDate(timeIntervalSince1970: 0)
-let date2 = NSDate(timeIntervalSince1970: 1839203982)
-let date3 = NSDate(timeIntervalSince1970: 1339203982)
-
-date1 < date2 // true
-date0 == date1 // true
-date3 > date2 // false
-
-var hello: [String] = ["hello", "two"]
-find(hello, "hello")
